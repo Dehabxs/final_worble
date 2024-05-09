c3c825a3575ccfe522727d98427f9edc79071e71
zach vann <vannzach727@gmail.com>
2024-05-09 14:11:50 -0700

    websight
    websight

---
+++ scripts/home.js
@@ -0,0 +1,26 @@
+document.querySelectorAll('#lol a').forEach(item => {
+    item.addEventListener('mouseover', () => {
+        item.style.textShadow = '0 0 20px #00ffff';
+    });
+    item.addEventListener('mouseout', () => {
+        item.style.textShadow = 'none';
+    });
+});
+
+
+// Function to create stars
+function createStars() {
+    const starsNumber = 150;
+    for (let i = 0; i < starsNumber; i++) {
+        const star = document.createElement('div');
+        star.className = 'star';
+        star.style.left = `${Math.random() * window.innerWidth}px`;
+        star.style.top = `${Math.random() * window.innerHeight}px`;
+        star.style.animationDuration = `${Math.random() * 2 + 1}s`; // Randomize animation speed
+        star.style.width = `${Math.random() * 2 + 1}px`; // Randomize star size
+        star.style.height = star.style.width;
+        document.body.appendChild(star);
+    }
+}
+
+window.onload = createStars;
